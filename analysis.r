7010bd56ff1f3887395c4afda79b2d6567e3822f
inamulhaqazad <inamulhaq.haq798@gmail.com>
2026-02-21 12:45:30 +0530

    Completed analysis.r, data combined and generated boxplot of expression by cell type; saved plot to results directory

--- analysis.r
+++ analysis.r
@@ -19,12 +19,13 @@
 # Load libraries -------------------
 # You may use base R or tidyverse for this exercise
 
-# ex. library(tidyverse)
+library(tidyverse)
 
 # Load data here ----------------------
 # Load each file with a meaningful variable name.
 
-
+expression_data <- read.csv("data/GSE60450_GeneLevel_Normalized(CPM.and.TMM)_data.csv", row.names = 1)
+metadata <- read.csv("data/GSE60450_filtered_metadata.csv")
 
 # Inspect the data -------------------------
 
@@ -33,20 +34,58 @@
 
 ## Expression data
 
+dim(expression_data)
+head(expression_data)
+str(expression_data)
 
 ## Metadata
+
+dim(metadata)
+head(metadata)
+str(metadata)
 
 
 # Prepare/combine the data for plotting ------------------------
 # How can you combine this data into one data.frame?
 
+# Convert expression matrix to long format
+expression_long <- expression_data %>% select(-gene_symbol) %>%
+  rownames_to_column(var = "gene") %>%
+  pivot_longer(
+    cols = -gene,
+    names_to = "sample",
+    values_to = "expression"
+  )
 
+# Merge with metadata by sample name
+combined_data <- expression_long %>%
+  left_join(metadata, by = c("sample" = "X"))
+
+# Inspect combined data
+dim(combined_data)
+head(combined_data)
 
 # Plot the data --------------------------
 ## Plot the expression by cell type
 ## Can use boxplot() or geom_boxplot() in ggplot2
 
-
+ggplot(combined_data, aes(x = sample, y = expression)) +
+  geom_boxplot() +
+  labs(title = "Gene Expression by Cell Type",
+       x = "Cell Type",
+       y = "Expression") +
+  theme_minimal()+
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
 
 ## Save the plot
 ### Show code for saving the plot with ggsave() or a similar function
+
+# Save plot
+ggsave(
+  filename = "results/expression_by_cell_type.png",
+  width = 8,
+  height = 6,
+  dpi = 300
+)
+
